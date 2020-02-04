3342d95ee70b05f09edd8c3c9f7c13e1b7bea700
Gašper <gasperbr@proxima.si>
2020-02-04 14:54:39 +0100

    update file type

--- main.cjs
+++ main.cjs
@@ -1,3 +1,9 @@
+/* import dotenv from 'dotenv';
+import cron from 'cron';
+import binance from 'binance-api-node';
+dotenv.config();
+const CronJob = cron.CronJob;
+const Binance = binance; */
 require('dotenv').config();
 const CronJob = require('cron').CronJob;
 const Binance = require('binance-api-node').default;
@@ -5,18 +11,14 @@ const Binance = require('binance-api-node').default;
 const environment = {
 	apiSecret: process.env.BINANCE_API_SECRET,
 	apiKey: process.env.BINANCE_API,
-	profitPercent: int(process.env.PROFIT_PERCENT),
+	profitPercentMin: float(process.env.PROFIT_PERCENT_MIN),
+	profitPercentMax: float(process.env.PROFIT_PERCENT_MAX),
+	sellSpreadMax: float(process.env.SELL_SPREAD_MAX),
 	everyXHours: int(process.env.EVERY_X_HOURS),
 	minOrderUsdt: float(process.env.MIN_ORDER_USDT),
 	minOrderPercent: int(process.env.MIN_ORDER_SIZE_OF_CURRENT_BALANCE),
 	priceDecimals: int(process.env.PRICE_DECIMALS),
 	nanoDecimals: int(process.env.NANO_AMOUNT_DECIMALS)
-}
-
-for (let key in environment) {
-	if (!environment[key] && environment[key] !== 0) {
-		return;
-	}
 }
 
 const client = Binance({
@@ -28,31 +30,49 @@ let sellExtraNanosOnNextRound = {
 	amount: 0,
 	boughtAt: 0,
 }
-console.log('------------------------------------------');
-/* let i = 1;
-new CronJob('* * * * *', function() {
-	console.log(i);
-  if (i % environment.everyXHours === 0) {
-		i = 1;
-		nanoUsdtBuySell();
-  } else {
-		i++;
-  }
-}, null, true, 'America/Los_Angeles'); */
 
-nanoUsdtBuySell();
+new CronJob('*/20 * * * *', function() {
+  checkForEmptySellSide();
+}, null, true, 'America/Los_Angeles');
+
+function checkForEmptySellSide() {
+	Promise.all([client.openOrders({symbol: 'NANOUSDT'}), getBook()]).then(array => {
+		const sellOrders = (array[0] || []).filter(o => o.side === 'SELL').map(o => float(o.price));
+		const book = array[1];
+
+    if (sellOrders.length === 0) {
+			console.log('no other orders, run bot');
+			nanoUsdtBuySell();
+		} else {
+			let minOrderPrice = sellOrders[0];
+			sellOrders.forEach(price => {
+				if (price < minOrderPrice) {
+					minOrderPrice = price; // get lowest sell order
+				}
+			});
+			const percentDifference = (minOrderPrice / book.lowestAsk - 1) * 100;
+			const maxDiff = environment.sellSpreadMax;
+			if (percentDifference > maxDiff) {
+        console.log(`difference lowest ask / my lowest sell (${percentDifference}) too low (max: ${maxDiff}), runnung bot`);
+				nanoUsdtBuySell();
+      }
+		}
+	}).catch(err => {
+		console.log('could not check sell orders ', err);
+	});
+}
 
 async function nanoUsdtBuySell() {
 	
-	const book = await getBook();
+  const book = await getBook();
 	if (book.usdtBalance < environment.minOrderUsdt) {
 		return;
 	}
 
-	tryLimitOrder(book).then((order: any) => {
+	tryLimitOrder(book).then((order) => {
 		if (order.status === 'FILLED') {
 
-			console.log(`Bought ${order.executedQty} nanos @${order.price}`);
+			console.log(`Bought ${order.executedQty} nanos @${order.price}  ($${order.executedQty * order.price})`);
 			setLimitSellOrder(float(order.executedQty), float(order.price));
 
 		} else if (order.status === 'PARTIALLY_FILLED') {
@@ -102,8 +122,8 @@ async function getBook() {
 
 function marketBuyAndSetLimitSell() {
 	marketBuy().then((marketOrder) => {
-		console.log('market order', marketOrder);
 		const price = getHighestFillPrice(marketOrder);
+		console.log(`market bought ${marketOrder.executedQty} nanos @ ${price} ($${marketOrder.executedQty * price})`)
 		setLimitSellOrder(float(marketOrder.executedQty), price);
 	}).catch(err => {
 		console.log('could not market buy', err);
@@ -114,10 +134,15 @@ function getHighestFillPrice(marketOrder) {
 	return float(marketOrder.fills.pop().price);
 }
 
+function getProfit(min=2, max=2) {
+	const actualPercent = min + ((max - min) * Math.random());
+	return (actualPercent + 100) / 100; // in form of 1.023
+}
+
 function setLimitSellOrder(nanoToSell, boughtAt) {
-	
+  const profit = getProfit(environment.profitPercentMin, environment.profitPercentMax);
 	nanoToSell = round(nanoToSell * 0.999, environment.nanoDecimals); // keep 1c of nano
-	let sellAt = round(boughtAt * ((environment.profitPercent + 100) / 100), environment.priceDecimals);
+	let sellAt = round(boughtAt * profit, environment.priceDecimals);
 
 	if (sellExtraNanosOnNextRound.amount > 0) {
 		nanoToSell += sellExtraNanosOnNextRound.amount;
@@ -131,7 +156,7 @@ function setLimitSellOrder(nanoToSell, boughtAt) {
 		price: sellAt,
 	};
 	client.order(orderData).then(order => {
-		console.log(`set sell order @${sellAt} for ${nanoToSell} nanos`);
+		console.log(`set sell order @ ${sellAt} for ${nanoToSell} nanos ($${sellAt*nanoToSell}) - profit: ${profit}`);
 		sellExtraNanosOnNextRound.amount = 0;
 		sellExtraNanosOnNextRound.boughtAt = 0;
 	}).catch(msg => {
@@ -141,7 +166,6 @@ function setLimitSellOrder(nanoToSell, boughtAt) {
 
 async function marketBuy() {
 	const book = await getBook();
-	console.log(book);
 	const orderData = {
 		symbol: 'NANOUSDT',
 		side: 'BUY',
@@ -154,9 +178,6 @@ async function marketBuy() {
 function getMarketBuyAmmount(book) {
 	const usdtAmmount = getUsdtBuyAmmount(book);
 	let nanos = getNanosForPrice(book.lowestAsk, usdtAmmount, environment.nanoDecimals);
-	if (nanos < book.lowestAskAmmount) {
-		nanos *= 1.01; // to avoid min amount error
-	}
 	return round(nanos, environment.nanoDecimals, 'UP'); 
 }
 
@@ -179,12 +200,12 @@ async function tryLimitOrder(book) {
 		};
 		client.order(orderData).then(order => {
 
-			setTimeout(() => { // wait 5s and return order
+			setTimeout(() => { // wait 15min and return order
 				client.getOrder({
 					symbol: 'NANOUSDT',
 					orderId: order.orderId,
 				}).then(resolve).catch(reject);
-			}, 5000);
+			}, 1000 * 60 * 15); // 15min
 
 		}).catch(reject);
 	});
@@ -195,7 +216,7 @@ function getNanosForPrice(price, usdt, nanoDecimals) {
 }
 
 function getUsdtBuyAmmount(book) {
-	return Math.max(environment.minOrderUsdt, book.usdtBalance * (environment.minOrderPercent / 100));
+	return Math.max(environment.minOrderUsdt, book.usdtBalance * environment.minOrderPercent / 100);
 }
 
 // HELPER FUNCTIONS
